c177bd9a9efdb89e709582d83b9bf782455e5b79
KorobejnikovAP <armok2001@mail.ru>
2024-03-01 13:42:09 +0300

    refactoring student handler

--- db/engine.py
+++ db/engine.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 from sqlalchemy import MetaData
 from sqlalchemy.orm import sessionmaker
 
-def create_async_engine(url: URL and str) -> AsyncEngine:
+def create_async_engine(url: URL | str) -> AsyncEngine:
     return _create_async_engine(url=url, echo=True)
 
 def get_session_maker(engine: AsyncEngine) -> sessionmaker:
--- db/record.py
+++ db/record.py
@@ -1,7 +1,7 @@
 from sqlalchemy.types import BigInteger, Integer, VARCHAR, DATE, TEXT
 from sqlalchemy.dialects.postgresql import BYTEA
-from sqlalchemy import Column, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey, select
+from sqlalchemy.orm import relationship, sessionmaker
 from .base import BaseModel
 
 
@@ -16,6 +16,13 @@ class Record(BaseModel):
     student = relationship("User", foreign_keys=[student_id])
     topic = Column(VARCHAR(50))
     data = Column(BYTEA)
+
+    @classmethod
+    async def get_student_records(self, session_maker: sessionmaker, id_student: int):
+        async with session_maker.begin() as session:
+            records = await session.scalars(select(Record).where(Record.student_id==id_student))
+            session.expunge_all()
+        return records
     
     def __str__(self) -> str:
         return f"Record id: {self.record_id}"
